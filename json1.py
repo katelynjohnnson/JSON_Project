4507b31750351f5ed9d09aba6c5f76ce3d4119b9
katelynjohnnson <89552485+katelynjohnnson@users.noreply.github.com>
2021-10-03 22:37:55 -0500

    json1 created

---
+++ json1.py
@@ -0,0 +1,26 @@
+import json
+
+infile = open('eq_data_1_day_m1.json', 'r')
+outfile = open('readable_eq_data.json', 'w')
+
+eq_data = json.load(infile)
+
+json.dump(eq_data, outfile, indent=4)
+
+list_of_eqs = eq_data['features']
+
+mags = []
+lons = []
+lats = []
+
+for eq in list_of_eqs:
+    mag = eq['properties']['mag']
+    lon = eq['geometry']['coordinates'][0]
+    lat = eq['geometry']['coordinates'][1]
+    mags.append(mag)
+    lons.append(lon)
+    lats.append(lat)
+
+print(mags)
+print(lons)
+print(lats)
